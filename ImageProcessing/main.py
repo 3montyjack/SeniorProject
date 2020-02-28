74e5c2d4aa07d4c56bd4f7ed2d8983167a41c827
Jack <3montyjack@gmail.com>
2020-02-28 12:30:59 -0500

    Start of the project

---
+++ ImageProcessing/main.py
@@ -0,0 +1,10 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import pandas as pd
+import tensorflow as tf
